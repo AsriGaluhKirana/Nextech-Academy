8c8d5639b1bb1d68f7eb26ad667e83f017cac954
asrigaluh <124650175+AsriGaluhK@users.noreply.github.com>
2023-08-22 14:55:33 +0700

    add ui detail course

--- js/main.js
+++ js/main.js
@@ -230,31 +230,31 @@ $(document).ready(function () {
     });
 
     // Detail course Accordion
-    $(document).ready(function() {
+    // $(document).ready(function() {
 
-        $(".toggle-accordion").on("click", function() {
-          var accordionId = $(this).attr("accordion-id"),
-            numPanelOpen = $(accordionId + ' .collapse.in').length;
+    //     $(".toggle-accordion").on("click", function() {
+    //       var accordionId = $(this).attr("accordion-id"),
+    //         numPanelOpen = $(accordionId + ' .collapse.in').length;
           
-          $(this).toggleClass("active");
+    //       $(this).toggleClass("active");
       
-          if (numPanelOpen == 0) {
-            openAllPanels(accordionId);
-          } else {
-            closeAllPanels(accordionId);
-          }
-        })
+    //       if (numPanelOpen == 0) {
+    //         openAllPanels(accordionId);
+    //       } else {
+    //         closeAllPanels(accordionId);
+    //       }
+    //     })
       
-        openAllPanels = function(aId) {
-          console.log("setAllPanelOpen");
-          $(aId + ' .panel-collapse:not(".in")').collapse('show');
-        }
-        closeAllPanels = function(aId) {
-          console.log("setAllPanelclose");
-          $(aId + ' .panel-collapse.in').collapse('hide');
-        }
+    //     openAllPanels = function(aId) {
+    //       console.log("setAllPanelOpen");
+    //       $(aId + ' .panel-collapse:not(".in")').collapse('show');
+    //     }
+    //     closeAllPanels = function(aId) {
+    //       console.log("setAllPanelclose");
+    //       $(aId + ' .panel-collapse.in').collapse('hide');
+    //     }
            
-      });
+    //   });
 
 });
 
